248e8886700febe8462b0fe7d18815277080d002
Mitzimberg <BlakeMitzimberg003@gmail.com>
2024-10-31 05:20:12 -0500

    Lots of work

--- js/main.js
+++ js/main.js
@@ -1,1 +1,75 @@
-// JS scripts placed here
+function disableScroll() {
+    window.scrollTo(0, 0);
+}
+
+window.addEventListener('scroll', disableScroll);
+window.addEventListener('wheel', disableScroll, { passive: false });
+window.addEventListener('touchmove', disableScroll, { passive: false });
+
+//ngl javascript is byfar my weakest trait, all of this is just ripped lol
+
+const surfboard = document.getElementById('Surfboard');
+const crab = document.getElementById('wholeCrab');
+let moved = false;
+
+surfboard.addEventListener('click', () => {
+    if (!moved) {
+        crab.style.transform = 'translateX(-150px)';
+    } else {
+        crab.style.transform = 'translateX(0px)';
+    }
+    moved = !moved;
+});
+
+const paradiseElement = document.getElementById('Paradise');
+const cloudsElement = document.getElementById('Cloud1');
+const cloudsElement2 = document.getElementById('Cloud_2')
+paradiseElement.style.pointerEvents = 'none';
+
+setTimeout(() => {
+    paradiseElement.style.pointerEvents = 'auto';
+    
+    const handleClick = () => {
+        paradiseElement.animate([
+            { transform: 'translateY(0)' },
+            { transform: 'translateY(-100vh)' }
+        ], {
+            duration: 1000,
+            fill: 'forwards'
+        }).onfinish = () => {
+            startCloudMovement();
+            startCloudMovement2();
+        };
+
+        paradiseElement.style.pointerEvents = 'none';
+        paradiseElement.removeEventListener('click', handleClick);
+    };
+    paradiseElement.addEventListener('click', handleClick);
+}, 5000);
+
+function startCloudMovement() {
+    cloudsElement.animate([
+        { transform: 'translateX(0)' },
+        { transform: 'translateX(200vw)' }
+    ], {
+        duration: 35000,
+    }); 
+}
+function startCloudMovement2() {
+    cloudsElement2.animate([
+        { transform: 'translateX(0)' },
+        { transform: 'translateX(-200vw)' }
+    ], {
+        duration: 60000,
+    });
+}
+
+const lifePreserver = document.getElementById('LifePreserver');
+
+lifePreserver.addEventListener('click', () => {
+    lifePreserver.style.transform = 'scale(1.02)';
+
+    setTimeout(() => {
+        lifePreserver.style.transform = 'scale(1)';
+    }, 300);
+});
